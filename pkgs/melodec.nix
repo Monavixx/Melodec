0eaeb3b44e731f1b9e1e8b432472ecff1638c6af
monavixx <dperelygin0@gmail.com>
2026-08-20 15:15:20 +0300

    fix bin output

--- pkgs/melodec.nix
+++ pkgs/melodec.nix
@@ -11,18 +11,14 @@ pkgs.buildDotnetModule rec {
   src = ../melodec-tool;
   projectFile = "../melodec-tool/melodec-tool.csproj";
 
-  # Point to your local placeholder file
-  # nugetDeps = inputs.nuget-packageslock2nix.lib {
-  #   system = "x86_64-linux";
-  #   name = pname;
-  #   lockfiles = [ ../melodec-tool/packages.lock.json ];
-  # };
   nugetDeps = ../melodec-tool/deps.json;
 
   dotnet-sdk = pkgs.dotnetCorePackages.sdk_10_0;
   dotnet-runtime = pkgs.dotnetCorePackages.runtime_10_0;
   nativeBuildInputs = [ pkgs.clang ];
   buildInputs = [ pkgs.zlib ];
-  # executables = [ "melodec" ];
   selfContainedBuild = true;
+  dotnetFlags = [
+    "-p:StripSymbols=false"
+  ];
 }
